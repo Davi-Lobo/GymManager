cb54ec660ccf1b31b90ea3366072c04869e0804a
Davi Lobo <davi.lobo.gs@gmail.com>
2020-03-04 00:04:36 -0300

    Route /instructors/create

--- routes.js
+++ routes.js
@@ -9,6 +9,10 @@ routes.get('/instructors', function(req, res) {
     return res.send('instructors');
 });
 
+routes.get('/instructors/create', function(req, res) {
+    return res.render('instructors/create');
+});
+
 routes.get('/members', function(req, res) {
     return res.send('members');
 });
