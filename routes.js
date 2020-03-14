cc35ca5fb35402fb0ac38334e7b25376b4b1ab70
Davi Lobo <davi.lobo.gs@gmail.com>
2020-03-14 00:30:36 -0300

    Form fields are now a template and edit page creation

--- routes.js
+++ routes.js
@@ -19,6 +19,10 @@ routes.get('/instructors/create', function(req, res) {
 
 routes.get('/instructors/:id', instructors.show);
 
+routes.get('/instructors/:id/edit', function(req, res) {
+    return res.render('instructors/edit')
+});
+
 routes.get('/members', function(req, res) {
     return res.send('members'); 
 });
