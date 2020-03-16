9be28102bc135428f0138531662c95849345fad5
Davi Lobo <davi.lobo.gs@gmail.com>
2020-03-15 23:59:08 -0300

    method-override installed and instructor id in edit page

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
+const methodOverride = require('method-override');
 
 const routes = require('./routes');
 
@@ -7,6 +8,7 @@ const server = express();
 
 server.use(express.urlencoded({ extended: true }));
 server.use(express.static('public'));
+server.use(methodOverride('_method'));
 server.use(routes);
 
 server.set("view engine", "njk");
